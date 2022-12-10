c04da1f680d7a65ed4b614a0598c67a708e540dd
DJE98 <dennis@eisermann.one>
2022-12-10 09:02:57 +0100

    Added create extract folder path function

--- texparser/tar_extractor.py
+++ texparser/tar_extractor.py
@@ -8,3 +8,9 @@ class TarExtractor:
         self.extract_folder_path = extract_folder_path
         self.dataset_folder_path = dataset_folder_path
 
+    def create_extract_folder_path(self) -> None:
+        if not os.path.exists(self.extract_folder_path) and self.extract_folder_path != "":
+            os.mkdir(self.extract_folder_path)
+        if not os.path.exists(self.extract_folder_path + self.dataset_folder_path) and self.dataset_folder_path != "":
+            os.mkdir(self.extract_folder_path + self.dataset_folder_path)
+
