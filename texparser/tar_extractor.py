3579f7bf699aecbea801c7d5738210435637ae8a
DJE98 <dennis@eisermann.one>
2022-12-10 09:19:54 +0100

    Added private create tar process function

--- texparser/tar_extractor.py
+++ texparser/tar_extractor.py
@@ -16,6 +16,9 @@ class TarExtractor:
             os.mkdir(file_folder_path)
         return file_folder_path
 
+    def __create_tar_process(self, params: str, file_name: str, file_folder_path: str):
+        return subprocess.run(["tar", params, file_name, "--directory", file_folder_path], capture_output=True)
+
     def create_extract_folder_path(self) -> None:
         if not os.path.exists(self.extract_folder_path) and self.extract_folder_path != "":
             os.mkdir(self.extract_folder_path)
