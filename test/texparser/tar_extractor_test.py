a83ddcb572bf739163b46ef16cdb7fbd968849b7
DJE98 <dennis@eisermann.one>
2022-12-12 14:00:29 +0100

    Added test for create extract empty folder path with empty path

--- test/texparser/tar_extractor_test.py
+++ test/texparser/tar_extractor_test.py
@@ -11,3 +11,9 @@ class TarExtractorTest(unittest.TestCase):
         self.tar_extractor_with_extract_folder.create_extract_folder_path()
         filenames = os.listdir(".")
         self.assertIn("extract", filenames)
+
+    def test_create_extract_empty_folder_path(self):
+        old_filenames = os.listdir(".")
+        self.tar_extractor_without_extract_folder.create_extract_folder_path()
+        filenames = os.listdir(".")
+        self.assertEqual(old_filenames, filenames)
