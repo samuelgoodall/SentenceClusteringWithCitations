40708fbcb53f49910f23ad02865996e950e39199
jana370 <jana.eiting@gmx.net>
2022-12-13 13:19:48 +0100

    Added test length_related_work without input str

--- test/texparser/information_extractor_test.py
+++ test/texparser/information_extractor_test.py
@@ -9,3 +9,7 @@ class InformationExtractorTest(unittest.TestCase):
         self.related_work_symbol = "\section{Related Work}"
         self.boiler_plate = "sdfsjfdkjs djfhskj dhfksh"
         self.next_section_symbol = "\section{Next Section}"
+
+    def test_get_length_related_work_no_string(self):
+        related_work_length = self.information_extractor._InformationExtractor__get_length_related_work("", 5503)
+        self.assertEqual(-1, related_work_length)
