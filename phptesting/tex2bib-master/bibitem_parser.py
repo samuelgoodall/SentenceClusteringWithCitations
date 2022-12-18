a292d120e3f5b2ba6267ffee9842d1654f4a28dd
Samuel Goodall <samuel.goodall@exxcellent.de>
2022-12-18 17:56:00 +0100

    feat: -added documentation to bibitem_parser.py

--- phptesting/tex2bib-master/bibitem_parser.py
+++ phptesting/tex2bib-master/bibitem_parser.py
@@ -1,15 +1,18 @@
 import string
 import subprocess
 import bibtexparser
+
 def convert_texfile_2_bib_entries(php_convertion_script_file, tex_input_file):
     """
-            Parameters
-            ----------
-            php_convertion_script_file : str
-                the path to the convertion script file
-            tex_input_file : str
-                tex file with the bibitems that are to be converted
-            """
+    Uses this Bib 2 convert tex2bib: https://github.com/juusechec/tex2bib
+    that is based on :https://text2bib.economics.utoronto.ca/index.php/index
+    Parameters
+    ----------
+    php_convertion_script_file : str
+        the path to the convertion script file
+    tex_input_file : str
+        tex file with the bibitems that are to be converted
+    """
     result = subprocess.run(
         ['php', php_convertion_script_file,
          tex_input_file],  # program and arguments
