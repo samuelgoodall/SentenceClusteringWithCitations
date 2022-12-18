099c1a070bcbe11f605401228ece4d0002308b56
Samuel Goodall <samuel.goodall@exxcellent.de>
2022-12-18 17:43:29 +0100

    feat: -added bibitem_parser.py can now parse bibitems from a texfile -modified index.php to accept command line argument with filepath to read specific tex file

---
+++ phptesting/tex2bib-master/bibitem_parser.py
@@ -0,0 +1,29 @@
+import string
+import subprocess
+import bibtexparser
+def convert_texfile_2_bib_entries(php_convertion_script_file, tex_input_file):
+    """
+            Parameters
+            ----------
+            php_convertion_script_file : str
+                the path to the convertion script file
+            tex_input_file : str
+                tex file with the bibitems that are to be converted
+            """
+    result = subprocess.run(
+        ['php', php_convertion_script_file,
+         tex_input_file],  # program and arguments
+        text=True,
+        capture_output=True,
+        check=True  # raise exception if program fails
+    )
+    resultstring: string = result.stdout
+    bib_entries = bibtexparser.loads(resultstring).entries
+    return bib_entries
+
+
+if __name__ == "__main__":
+    tex_input_file = '/home/samuel/Desktop/WS2223Projekt/NLPProjekt/phptesting/tex2bib-master/example-cites.tex'
+    php_convertion_script_file = '/home/samuel/Desktop/WS2223Projekt/NLPProjekt/phptesting/tex2bib-master/index.php'
+    bib_entries = convert_texfile_2_bib_entries(php_convertion_script_file=php_convertion_script_file, tex_input_file=tex_input_file)
+    print("BIBENTRIES:",bib_entries)
--- phptesting/tex2bib-master/index.php
+++ phptesting/tex2bib-master/index.php
@@ -9,8 +9,7 @@ $verbose = 1;
 $debug = 0;
 $itemSeparator = 'cr';
 $percentComment = 1;
-$filename = 'example-cites.tex';
-
+$filename = $argv[1];
 $convert = new Convert($labelStyle, $overrideLabels, $lineEndings, $verbose, $debug, $itemSeparator, $percentComment);
 $items = $convert->convertToBibtex($filename);
 $outputstring = $convert->formBibtex($items);
