23365eb20192bc2eabfb99518f60b5d0476c2a5d
samuelgoodall <samuel.goodall@bcxp.de>
2022-12-19 10:24:48 +0100

    added new function to count how many titles are usabale

--- phptesting/tex2bib-master/bibitem_parser.py
+++ phptesting/tex2bib-master/bibitem_parser.py
@@ -1,6 +1,19 @@
+import json
 import string
 import subprocess
 import bibtexparser
+
+
+class Bibentry(object):
+    def __init__(self, publisher:str, address:str,year:str,title:str,author:str,ENTRYTYPE:str,ID:str):
+        self.publisher = publisher
+        self.address = address
+        self.year = year
+        self.title = title
+        self.author = author
+        self.ENTRYTPE = ENTRYTYPE
+        self.ID = ID
+
 
 def convert_texfile_2_bib_entries(php_convertion_script_file, tex_input_file):
     """
@@ -22,11 +35,32 @@ def convert_texfile_2_bib_entries(php_convertion_script_file, tex_input_file):
     )
     resultstring: string = result.stdout
     print(resultstring)
+    print("NEWLINES",resultstring.count('\n'))
     bib_entries = bibtexparser.loads(resultstring).entries
     return bib_entries
 
+def check_how_many_titles_are_usable(bibentrie_title_list: list):
+    accepted = 0
+    for i,bibentry in enumerate(bibentrie_title_list):
+        if(bibentrie_title_list!=""):
+            accepted += 1
+    i=i+1
+    acceptancerate = accepted/i
+
+    return i,accepted,acceptancerate
+
+
+def get_list_of_titles(bib_entries):
+    def helper(bibentry:dict):
+        return bibentry['title']
+    return list(map(helper,bib_entries))
+
+
 if __name__ == "__main__":
-    tex_input_file = '/home/samuel/Desktop/WS2223Projekt/NLPProjekt/phptesting/tex2bib-master/example-cites.tex'
-    php_convertion_script_file = '/home/samuel/Desktop/WS2223Projekt/NLPProjekt/phptesting/tex2bib-master/index.php'
+    tex_input_file = '/mnt/c/Users/sgoodall/Desktop/archive/NLPProjekt/phptesting/tex2bib-master/example-cites.tex'
+    php_convertion_script_file = '/mnt/c/Users/sgoodall/Desktop/archive/NLPProjekt/phptesting/tex2bib-master/index.php'
     bib_entries = convert_texfile_2_bib_entries(php_convertion_script_file=php_convertion_script_file, tex_input_file=tex_input_file)
-    print("BIBENTRIES:",bib_entries)
+    print(len(bib_entries))
+    print("Usable Titles",check_how_many_titles_are_usable(get_list_of_titles(bib_entries)))
+    print(len(get_list_of_titles(bib_entries)))
+
