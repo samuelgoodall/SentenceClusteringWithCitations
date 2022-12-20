8afe8664c672e3936d628ac4d35766bb11440cc6
DJE98 <dennis@eisermann.one>
2022-12-20 14:42:11 +0100

    Bugfixing wrong function calls

--- test/texparser/information_extractor_test.py
+++ test/texparser/information_extractor_test.py
@@ -1,7 +1,9 @@
-import unittest
 import sys
+import unittest
+
 sys.path.append(".")
 from texparser.information_extractor import InformationExtractor
+
 
 class InformationExtractorTest(unittest.TestCase):
     def setUp(self) -> None:
@@ -11,22 +13,22 @@ class InformationExtractorTest(unittest.TestCase):
         self.next_section_symbol = "\section{Next Section}"
 
     def test_length_related_work_no_string(self):
-        related_work_length = self.information_extractor._InformationExtractor__length_related_work("", 5503)
+        related_work_length = self.information_extractor.length_related_work("", 5503)
         self.assertEqual(-1, related_work_length)
 
     def test_length_related_work_no_related_work(self):
         test_string = self.boiler_plate + self.next_section_symbol
-        related_work_length = self.information_extractor._InformationExtractor__length_related_work(test_string, -1)
+        related_work_length = self.information_extractor.length_related_work(test_string, -1)
         self.assertEqual(-1, related_work_length)
 
     def test_length_related_work_no_following_section(self):
         test_string = self.related_work_symbol + self.boiler_plate
-        related_work_length = self.information_extractor._InformationExtractor__length_related_work(test_string, 0)
+        related_work_length = self.information_extractor.length_related_work(test_string, 0)
         self.assertEqual(len(test_string) - 3, related_work_length)
 
     def test_length_related_work_with_proper_related_work(self):
         test_string = self.related_work_symbol + self.boiler_plate +self.next_section_symbol
-        related_work_length = self.information_extractor._InformationExtractor__length_related_work(test_string, 0)
+        related_work_length = self.information_extractor.length_related_work(test_string, 0)
         real_related_work_length = len(self.boiler_plate)
         self.assertEqual(real_related_work_length, related_work_length)
 
