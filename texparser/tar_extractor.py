e2c7d29f21565defb9aea9d2d8309cdec2954186
jana370 <jana.eiting@gmx.net>
2023-01-09 17:57:14 +0100

    changed private functions to have one _

--- texparser/tar_extractor.py
+++ texparser/tar_extractor.py
@@ -21,18 +21,18 @@ class TarExtractor:
             except FileExistsError:
                 sys.stderr.write("Error message: File already exists. \n")
         return processes
-    def extract_file_into_folder(self, file_name: str, params: str, ending: str) -> tuple:
+    def _extract_file_into_folder(self, file_name: str, params: str, ending: str) -> tuple:
         tar_process = any
         if file_name.endswith(ending):
             if ending in file_name:
-                file_folder_path = self.create_file_folder_path(
+                file_folder_path = self._create_file_folder_path(
                     file_name, ending)
-                process = self.__create_tar_process(
+                process = self._create_tar_process(
                     params, file_name, file_folder_path)
-                return self.__analyse_process(process)
+                return self._analyse_process(process)
         return (0, 0)
 
-    def create_file_folder_path(self, file_name: str, ending: str) -> str:
+    def _create_file_folder_path(self, file_name: str, ending: str) -> str:
         file_folder_path = file_name.replace(ending, "")
         if not file_folder_path.startswith(self.extract_folder_path):
             file_folder_path = Path(self.extract_folder_path) / file_folder_path
@@ -44,10 +44,10 @@ class TarExtractor:
         pass
         return file_folder_path
 
-    def __create_tar_process(self, params: str, file_name: str, file_folder_path: str):
+    def _create_tar_process(self, params: str, file_name: str, file_folder_path: str):
         return subprocess.run(["tar", params, file_name, "--directory", file_folder_path], capture_output=True)
 
-    def __analyse_process(self, process):
+    def _analyse_process(self, process):
         fail_counter, success_counter = 0, 0
         if process.returncode == 0 or process.returncode == 2:
             success_counter = success_counter + 1
@@ -56,10 +56,10 @@ class TarExtractor:
         return (success_counter, fail_counter)
 
     def untargz_file_into_folder(self, filename: str):
-        return self.extract_file_into_folder(filename, "xf", ".gz")
+        return self._extract_file_into_folder(filename, "xf", ".gz")
 
     def untar_file_into_folder(self, file_name: str):
-        return self.extract_file_into_folder(file_name, "xvf", ".tar")
+        return self._extract_file_into_folder(file_name, "xvf", ".tar")
 
     def create_extract_folder_path(self) -> None:
         try:
