4ec5ba4ddf91eed0f3f0ef57582ab22393d4e1f4
jana370 <jana.eiting@gmx.net>
2023-01-09 17:57:47 +0100

    updated functions names to have _

--- test/texparser/tar_extractor_test.py
+++ test/texparser/tar_extractor_test.py
@@ -26,7 +26,7 @@ class TarExtractorTest(unittest.TestCase):
 
     def test_create_file_folder_path_with_empty_extract_folder(self):
         example_file = "arXiv_src_1705_016.tar"
-        self.tar_extractor_without_extract_folder.__create_file_folder_path(example_file, ".tar")
+        self.tar_extractor_without_extract_folder._create_file_folder_path(example_file, ".tar")
         filenames = os.listdir(self.tar_extractor_without_extract_folder.dataset_folder_path)
         self.assertIn(example_file.replace(".tar", ""), filenames)
 
@@ -34,7 +34,7 @@ class TarExtractorTest(unittest.TestCase):
         example_file = "arXiv_src_1705_016.tar"
         example_folder = "filename"
         self.tar_extractor_with_extract_folder.create_extract_folder_path()
-        self.tar_extractor_with_extract_folder.__create_file_folder_path(example_file, ".tar")
+        self.tar_extractor_with_extract_folder._create_file_folder_path(example_file, ".tar")
         path_to_folder = os.path.join(self.tar_extractor_with_extract_folder.extract_folder_path, self.tar_extractor_with_extract_folder.dataset_folder_path)
         filenames = os.listdir(path_to_folder)
         self.assertIn(example_folder, filenames)
@@ -44,7 +44,7 @@ class TarExtractorTest(unittest.TestCase):
         example_folder = example_file.replace(".tar", "")
         self.tar_extractor_with_extract_folder.create_extract_folder_path()
         #t = TarExtractor("content", "extract")
-        self.tar_extractor_with_extract_folder.__extract_file_into_folder(example_file, "filler", ".tar")
+        self.tar_extractor_with_extract_folder._extract_file_into_folder(example_file, "filler", ".tar")
         path_to_folder = os.path.join(self.tar_extractor_with_extract_folder.extract_folder_path, self.tar_extractor_with_extract_folder.dataset_folder_path)
         filenames = os.listdir(path_to_folder)
         self.assertIn(example_folder, filenames)
