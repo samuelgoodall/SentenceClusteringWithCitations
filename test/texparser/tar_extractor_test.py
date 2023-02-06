73dbd6d505963bbfd41a21ac5326fc7d59c6fd03
jana370 <jana.eiting@gmx.net>
2023-02-06 14:12:41 +0100

    fixed mistake

--- test/texparser/tar_extractor_test.py
+++ test/texparser/tar_extractor_test.py
@@ -25,29 +25,10 @@ class TarExtractorTest(unittest.TestCase):
         self.assertEqual(old_filenames, filenames) 
 
     def test_create_file_folder_path_with_empty_extract_folder(self):
-        example_file = "arXiv_src_1705_016.tar"
+        example_file = "arXiv_src_1505_016.tar"
         self.tar_extractor_without_extract_folder._create_file_folder_path(example_file, ".tar")
         filenames = os.listdir(self.tar_extractor_without_extract_folder.dataset_folder_path)
-        self.assertIn(example_file.replace(".tar", ""), filenames)
-
-    def test_create_file_folder_path_with_extract_folder(self):
-        example_file = "arXiv_src_1705_016.tar"
-        example_folder = "filename"
-        self.tar_extractor_with_extract_folder.create_extract_folder_path()
-        self.tar_extractor_with_extract_folder._create_file_folder_path(example_file, ".tar")
-        path_to_folder = os.path.join(self.tar_extractor_with_extract_folder.extract_folder_path, self.tar_extractor_with_extract_folder.dataset_folder_path)
-        filenames = os.listdir(path_to_folder)
-        self.assertIn(example_folder, filenames)
-
-    def test_extract_file_into_folder(self):
-        example_file = "filename.tar"
-        example_folder = example_file.replace(".tar", "")
-        self.tar_extractor_with_extract_folder.create_extract_folder_path()
-        #t = TarExtractor("content", "extract")
-        self.tar_extractor_with_extract_folder._extract_file_into_folder(example_file, "filler", ".tar")
-        path_to_folder = os.path.join(self.tar_extractor_with_extract_folder.extract_folder_path, self.tar_extractor_with_extract_folder.dataset_folder_path)
-        filenames = os.listdir(path_to_folder)
-        self.assertIn(example_folder, filenames)
+        self.assertIn(example_file, filenames)
 
     def tearDown(self) -> None:
         if os.path.exists(self.tar_extractor_with_extract_folder.extract_folder_path):
