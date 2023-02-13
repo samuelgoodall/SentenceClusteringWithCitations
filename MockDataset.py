eb00b9406f7f23499f4b253c5f3fe796d7568a2c
jana370 <jana.eiting@gmx.net>
2023-02-13 13:40:06 +0100

    read data from csv file

--- MockDataset.py
+++ MockDataset.py
@@ -1,17 +1,22 @@
-from torch.utils.data import Dataset, DataLoader
+import csv
+
+from torch.utils.data import DataLoader, Dataset
+
 from qualitative_information_extractor import qualitativeInformationExtractor
 
 
 class MockDataset(Dataset):
 
-    def __init__(self):
-        extractor = qualitativeInformationExtractor()
-        extracted = qualitativeInformationExtractor.fill_data_set(extractor, "texparser/")
-        self.sentences = list(map(lambda item: item.get("sentence"), extracted))
-        print(self.sentences)
-
+    def __init__(self, csv_file: str):
+        filename = open(csv_file, "r", encoding = "utf8")
+        file = csv.DictReader(filename)
+        self.sentences = []
+        for col in file:
+            self.sentences.append(col['sentence'])
     def __getitem__(self, index):
         return self.sentences[index]
 
     def __len__(self):
         return len(self.sentences)
+    
+
