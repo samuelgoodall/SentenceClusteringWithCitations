f9f34a6a3d6c3bfe711392876e402b77496dc14e
esteli <emg96@uw.edu>
2023-03-10 13:08:22 +0100

    Added a BERT embedding class. Still need to finish the hyperparameters and test if it's working

---
+++ experiments/embedding_methods/bert_embedding.py
@@ -0,0 +1,29 @@
+import json
+from pathlib import Path
+
+import numpy
+import numpy as np
+from experiments.embedding_methods.embedding_interface import EmbeddingInterface
+from transformers import BertTokenizer, BertModel
+
+
+class BertTransformerEmbedding(EmbeddingInterface):
+
+    def __init__(self, model_name: str):
+        self.model = BertModel.from_pretrained("bert-base-uncased")
+        self.model_name = model_name
+        self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+
+    def embed_sentence(self, sentence: str):
+        """
+        sentence : str
+            the string that is to be embedded
+        """
+        encoded_input = self.tokenizer(sentence, return_tensors='pt')
+        output = self.model(**encoded_input)
+        return output
+
+    def return_hyper_params(self):
+        hyper_params = {"embedding_dimension": ,
+                        "model_name":}
+        return hyper_params
