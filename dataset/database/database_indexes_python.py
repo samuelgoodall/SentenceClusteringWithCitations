4d695bba4051e295e5fffce2291549defe000e4a
jana370 <jana.eiting@gmx.net>
2023-03-14 23:32:13 +0900

    to add indixes to database using python

---
+++ dataset/database/database_indexes_python.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+# Connect to the database
+conn = sqlite3.connect('dataset/database/dataset.db')
+
+# Create indexes
+conn.execute('CREATE INDEX paragraph_paper_index ON paragraph(paper_id)')
+conn.execute('CREATE INDEX sentence_paragraph_index ON sentence(paragraph_id)')
+conn.execute('CREATE UNIQUE INDEX sentence_citation_relation_index ON sentence_citation_relation(sentence_id,citation_id)')
+
+# Commit changes and close the connection
+conn.commit()
+conn.close()
+
+
+
+
+
+
