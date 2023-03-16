6175d3b05861b3f93aa61b07b6a0e26dfbb1e0b4
Sebastian Graf <sebastian.graf@uni-ulm.de>
2023-03-16 11:32:20 +0100

    Added AutomaticClusterFinder and documentations

--- experiments/clustering_methods/gmm_clustering.py
+++ experiments/clustering_methods/gmm_clustering.py
@@ -4,8 +4,67 @@ from sklearn.mixture import GaussianMixture
 
 class GMMClustering(ClusteringInterface):
     def __init__(self, n_components: int, covariance_type: str = "full", tol: float = 1e-3, reg_covar: float = 1e-6,
-                 max_iter: int = 100, n_init: int = 1, init_params: str = "k-means++", random_state=None,
-                 warm_start: bool = False):
+                 max_iter: int = 100, n_init: int = 1, init_params: str = "k-means++", weights_init=None,
+                 means_init=None, precisions_init=None, random_state=None,
+                 warm_start: bool = False, verbose: int = 0, verbose_interval: int = 10):
+        """Creates a GMMClustering object with the according parameters. The created GMM has the same parameters
+
+                  Attributes
+                  ----------
+                  n_components : int, required
+                      The number of mixture components.
+
+                  covariance_type : {‘full’, ‘tied’, ‘diag’, ‘spherical’}, default=’full’
+                      String describing the type of covariance parameters to use. Must be one of:
+                         ‘full’: each component has its own general covariance matrix.
+                         ‘tied’: all components share the same general covariance matrix.
+                         ‘diag’: each component has its own diagonal covariance matrix.
+                         ‘spherical’: each component has its own single variance.
+
+                  tol : float, default=1e-3
+                      The convergence threshold. EM iterations will stop when the lower bound average gain is below this threshold.
+
+                  reg_covar : float, default=1e-6
+                      Non-negative regularization added to the diagonal of covariance. Allows to assure that the covariance matrices are all positive.
+
+                  max_iter : int, default=100
+                      The number of EM iterations to perform.
+
+                  n_init : int, default=1
+                      The number of initializations to perform. The best results are kept.
+
+                  init_params : {‘kmeans’, ‘k-means++’, ‘random’, ‘random_from_data’}, default=’k-means++’
+                      The method used to initialize the weights, the means and the precisions. String must be one of:
+                         ‘kmeans’ : responsibilities are initialized using kmeans.
+                         ‘k-means++’ : use the k-means++ method to initialize.
+                         ‘random’ : responsibilities are initialized randomly.
+                         ‘random_from_data’ : initial means are randomly selected data points.
+
+                  weights_init : array-like of shape (n_components, ), default=None
+                      The user-provided initial weights. If it is None, weights are initialized using the init_params method.
+
+                  means_init : array-like of shape (n_components, n_features), default=None
+                      The user-provided initial means, If it is None, means are initialized using the init_params method.
+
+                  precisions_init : array-like, default=None
+                      The user-provided initial precisions (inverse of the covariance matrices). If it is None, precisions are initialized using the ‘init_params’ method. The shape depends on ‘covariance_type’:
+                      (n_components,)                        if 'spherical',
+                      (n_features, n_features)               if 'tied',
+                      (n_components, n_features)             if 'diag',
+                      (n_components, n_features, n_features) if 'full'
+
+                  random_state : int, RandomState instance or None, default=None
+                      Controls the random seed given to the method chosen to initialize the parameters (see init_params). In addition, it controls the generation of random samples from the fitted distribution (see the method sample). Pass an int for reproducible output across multiple function calls. See Glossary.
+
+                  warm_start : bool, default=False
+                      If ‘warm_start’ is True, the solution of the last fitting is used as initialization for the next call of fit(). This can speed up convergence when fit is called several times on similar problems. In that case, ‘n_init’ is ignored and only a single initialization occurs upon the first call. See the Glossary.
+
+                  verbose : int, default=0
+                      Enable verbose output. If 1 then it prints the current initialization and each iteration step. If greater than 1 then it prints also the log probability and the time needed for each step.
+
+                  verbose_interval : int, default=10
+                      Number of iteration done before the next print.
+                  """
         self.n_components = n_components
         self.covariance_type = covariance_type
         self.tol = tol
@@ -13,17 +72,61 @@ class GMMClustering(ClusteringInterface):
         self.max_iter = max_iter
         self.n_init = n_init
         self.init_params = init_params
+        self.weights_init = weights_init
+        self.means_init = means_init
+        self.precisions_init = precisions_init
         self.random_state = random_state
         self.warm_start = warm_start
+        self.verbose = verbose
+        self.verbose_interval = verbose_interval
 
     def cluster_sentences(self, sentences: list):
-        """clusters sentences using gmm"""
-        gmm = GaussianMixture(n_components=self.n_components).fit(sentences)
+        """Creates a sklearn.mixtures.GaussianMixture object with the attributes of the GMMCLustering class,
+        fit it to the data and returns the predicted cluster labels. If the number of smaples is less than the n_component
+        attribute a GMM with n_component = number of samples is created.
+
+            Parameters
+            ----------
+            sentences : list
+                Input data for which the cluster labels should be predicted via a GMM
+
+            Returns
+            -------
+            labels : list
+                a list with the corresponding predicted labels for the input data
+        """
+        num_samples = len(sentences)
+        if num_samples == 1:
+            return [0]
+        elif num_samples < self.n_components:
+            gmm = GaussianMixture(n_components=num_samples, covariance_type=self.covariance_type, tol=self.tol,
+                                  reg_covar=self.reg_covar, max_iter=self.max_iter, n_init=self.n_init,
+                                  weights_init=self.weights_init, means_init=self.means_init,
+                                  precisions_init=self.precisions_init,
+                                  init_params=self.init_params, random_state=self.random_state,
+                                  warm_start=self.warm_start, verbose=self.verbose,
+                                  verbose_interval=self.verbose_interval)
+        else:
+            gmm = GaussianMixture(n_components=self.n_components, covariance_type=self.covariance_type, tol=self.tol,
+                                  reg_covar=self.reg_covar, max_iter=self.max_iter, n_init=self.n_init,
+                                  weights_init=self.weights_init, means_init=self.means_init,
+                                  precisions_init=self.precisions_init,
+                                  init_params=self.init_params, random_state=self.random_state,
+                                  warm_start=self.warm_start, verbose=self.verbose,
+                                  verbose_interval=self.verbose_interval)
+        gmm = gmm.fit(sentences)
         labels = gmm.predict(sentences)
         return labels
 
     def return_hyper_params(self):
-        """returns the hyper params of the clustering algorithm"""
+        """Returns the attributes of the GMMClustering class, which are also the hyperparameter of the created GMM model in the cluster_sentence method.
+
+        Returns
+        -------
+        hyper_params : dict
+            a dictionary with the name of the hyperparameter and the corresponding value
+        """
+
         hyper_params = {"n_components": self.n_components, "covariance_type": self.covariance_type, "tol": self.tol,
                         "reg_covar": self.reg_covar, "max_iter": self.max_iter, "n_init": self.n_init,
                         "init_params": self.init_params, "random_state": self.random_state,
--- experiments/clustering_methods/spherical_kmeans_clustering.py
+++ experiments/clustering_methods/spherical_kmeans_clustering.py
@@ -4,91 +4,102 @@ from scipy.sparse import csr_matrix
 
 
 class SphericalKMeansClustering(ClusteringInterface):
-    """n_clusters : int, optional, default: 8
-            The number of clusters to form as well as the number of centroids to generate.
-        init : str or numpy.ndarray, default: 'similar_cut'
-            One of ['similar_cut', 'k-means++', 'random'] or an numpy.ndarray
-            Method for initialization,
-            - 'similar_cut'
-            It is an k-means initialization method for high-dimensional vector space + Cosine.
-            See `Improving spherical k-means for document clustering (Kim et al., 2020)` for detail.
-            - 'k-means++'
-            It selects initial cluster centers for k-means clustering in a smart way to speed up convergence.
-            See https://en.wikipedia.org/wiki/K-means%2B%2B for detail.
-            - 'random'
-            choose k observations (rows) at random from data for the initial centroids.
-            - If an ndarray is passed, it should be of shape (n_clusters, n_features) and gives the initial centers.
-        sparsity: str or None, default: None
-            One of ['sculley', 'minimum_df', None]
-            Method for preserving sparsity of centroids.
-            'sculley': L1 ball projection method.
-                Reference: David Sculley. Web-scale k-means clustering.
-                In Proceedings of international conference on World wide web,2010.
-                It requires two parameters `radius` and `epsilon`.
-                `radius`: default 10
-                `epsilon`: default 5
-            'minium_df': Pruning method. It drops elements to zero which lower
-                than beta / |DF(C)|. DF(C) is the document number of a cluster and
-                beta is constant parameter.
-                It requires one parameter `minimum_df_factor` a.k.a beta
-                `minimum_df_factor`: default 0.01
-        max_iter : int, default: 10
-            Maximum number of iterations of the k-means algorithm for a single run.
-            It does not need large number. k-means algorithms converge fast.
-        tol : float, default: 1e-4
-            Relative tolerance with regards to inertia to declare convergence
-        verbose : int, default 0
-            Verbosity mode.
-        random_state : int, RandomState instance or None, optional, default: None
-            If int, random_state is the seed used by the random number generator;
-            If RandomState instance, random_state is the random number generator;
-            If None, the random number generator is the RandomState instance used
-            by `numpy.random`.
-        debug_directory: str, default: None
-            When debug_directory is not None, model save three informations.
-            First one is logs. It contains iteration time, loss, and sparsity.
-            Second one is temporal cluster labels for all iterations. Third one
-            is temporal cluster centroid vector for all iterations.
-        algorithm : str, default None
-            Computation algorithm.
-            Ignored
-        max_similar: float, default: 0.5
-            'similar_cut initializer' argument. The initializer select a point randomly,
-            and then remove points within distance <= `max_similar` from candidates of
-            next centroid. It works only when you set `init`='similar_cut'.
-        alpha: float, default: 3.0
-            'similar_cut initializer' argument. |candidates of initial centroids| / `n_clusters`
-            It works only when you set `init`='similar_cut'.
-            `alpha` must be larger than 1.0
-        radius: float, default: 10.0
-            'sculley L1 projection' argument. It works only when you set `sparsity`='sculley'
-        epsilon: float, default: 5.0
-            'sculley L1 projection' argument. It works only when you set `sparsity`='sculley'
-        minimum_df_factor: float, default: 0.01
-            'minimum df L1 projection' argument. It works only when you set `sparsity`='minimum_df'
-            `minimum_df_factor` must be real number between (0, 1)
-        """
 
     def __init__(
-        self,
-        num_clusters: int,
-        init: str = "k-means++",
-        sparsity: str = None,
-        max_iter: int = 15,
-        tol: float = 1e-4,
-        verbose: int = 0,
-        max_similar: float = 0.5,
-        alpha: float = 3.0,
-        radius: float = 10.0,
-        epsilon: float = 5.0,
-        minimum_df_factor: float = 0.01,
+            self,
+            num_clusters: int,
+            init: str = "k-means++",
+            sparsity: str = None,
+            max_iter: int = 10,
+            tol: float = 1e-4,
+            verbose: int = 0,
+            random_state = None,
+            max_similar: float = 0.5,
+            alpha: float = 3.0,
+            radius: float = 10.0,
+            epsilon: float = 5.0,
+            minimum_df_factor: float = 0.01,
     ):
+        """ Creates a SphericalKmeansClustering object with the according parameters. These attributes are used for the
+        creation of the SphericalKmeans object
+
+                Parameters
+                ----------
+
+                num_clusters : int,
+                    The number of clusters to form as well as the number of centroids to generate.
+
+                init : str or numpy.ndarray, default: 'similar_cut'
+                    One of ['similar_cut', 'k-means++', 'random'] or an numpy.ndarray
+                    Method for initialization,
+                    - 'similar_cut'
+                    It is an k-means initialization method for high-dimensional vector space + Cosine.
+                    See `Improving spherical k-means for document clustering (Kim et al., 2020)` for detail.
+                    - 'k-means++'
+                    It selects initial cluster centers for k-means clustering in a smart way to speed up convergence.
+                    See https://en.wikipedia.org/wiki/K-means%2B%2B for detail.
+                    - 'random'
+                    choose k observations (rows) at random from data for the initial centroids.
+                    - If an ndarray is passed, it should be of shape (n_clusters, n_features) and gives the initial centers.
+
+                sparsity: str or None, default: None
+                    One of ['sculley', 'minimum_df', None]
+                    Method for preserving sparsity of centroids.
+                    'sculley': L1 ball projection method.
+                        Reference: David Sculley. Web-scale k-means clustering.
+                        In Proceedings of international conference on World wide web,2010.
+                        It requires two parameters `radius` and `epsilon`.
+                        `radius`: default 10
+                        `epsilon`: default 5
+                    'minium_df': Pruning method. It drops elements to zero which lower
+                        than beta / |DF(C)|. DF(C) is the document number of a cluster and
+                        beta is constant parameter.
+                        It requires one parameter `minimum_df_factor` a.k.a beta
+                        `minimum_df_factor`: default 0.01
+
+                max_iter : int, default: 10
+                    Maximum number of iterations of the k-means algorithm for a single run.
+                    It does not need large number. k-means algorithms converge fast.
+
+                tol : float, default: 1e-4
+                    Relative tolerance with regards to inertia to declare convergence
+
+                verbose : int, default 0
+                    Verbosity mode.
+
+                random_state : int, RandomState instance or None, optional, default: None
+                    If int, random_state is the seed used by the random number generator;
+                    If RandomState instance, random_state is the random number generator;
+                    If None, the random number generator is the RandomState instance used
+                    by `numpy.random`.
+
+                max_similar: float, default: 0.5
+                    'similar_cut initializer' argument. The initializer select a point randomly,
+                    and then remove points within distance <= `max_similar` from candidates of
+                    next centroid. It works only when you set `init`='similar_cut'.
+
+                alpha: float, default: 3.0
+                    'similar_cut initializer' argument. |candidates of initial centroids| / `n_clusters`
+                    It works only when you set `init`='similar_cut'.
+                    `alpha` must be larger than 1.0
+
+                radius: float, default: 10.0
+                    'sculley L1 projection' argument. It works only when you set `sparsity`='sculley'
+
+                epsilon: float, default: 5.0
+                    'sculley L1 projection' argument. It works only when you set `sparsity`='sculley'
+
+                minimum_df_factor: float, default: 0.01
+                    'minimum df L1 projection' argument. It works only when you set `sparsity`='minimum_df'
+                    `minimum_df_factor` must be real number between (0, 1)
+                """
         self.num_clusters = num_clusters
         self.max_iter = max_iter
         self.sparsity = sparsity
         self.init = init
         self.tol = tol
         self.verbose = verbose
+        self.random_state = random_state
         self.max_similar = max_similar
         self.alpha = alpha
         self.radius = radius
@@ -96,7 +107,19 @@ class SphericalKMeansClustering(ClusteringInterface):
         self.min_df_factor = minimum_df_factor
 
     def cluster_sentences(self, sentences: list):
-        """clusters sentences using spherical kmeans"""
+        """Creates a soyclustering.SphericalKmeans object with the attributes of the SphericalKmeansClustering class,
+        transforms the input sentences to a csr matrix and retruns the calculated labels.
+
+            Parameters
+            ----------
+            sentences : list
+                Input data for which the cluster labels should be predicted via a GMM
+
+            Returns
+            -------
+            labels : list
+                a list with the corresponding predicted labels for the input data
+        """
         spherical_kmeans = SphericalKMeans(
             n_clusters=self.num_clusters,
             max_iter=self.max_iter,
@@ -104,6 +127,7 @@ class SphericalKMeansClustering(ClusteringInterface):
             init=self.init,
             tol=self.tol,
             verbose=self.verbose,
+            random_state=self.random_state,
             max_similar=self.max_similar,
             alpha=self.alpha,
             radius=self.radius,
@@ -115,7 +139,14 @@ class SphericalKMeansClustering(ClusteringInterface):
         return spherical_kmeans.labels_
 
     def return_hyper_params(self):
-        """returns the hyper params of the clustering algorithm"""
+        """Returns the attributes of the GMMClustering class, which are also the hyperparameter of the created GMM model
+        in the cluster_sentence method.
+
+        Returns
+        -------
+        hyper_params : dict
+            a dictionary with the name of the hyperparameter and the corresponding value
+        """
         hyper_params = {
             "num_cluster": self.num_clusters,
             "max_iter": self.max_iter,
