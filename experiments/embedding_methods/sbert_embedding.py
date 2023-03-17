60acfd217292fee888b870863141033e7e399532
esteli <emg96@uw.edu>
2023-03-17 14:21:22 +0100

    added a few more parameters to the sbert embedding wrapper class

--- experiments/embedding_methods/sbert_embedding.py
+++ experiments/embedding_methods/sbert_embedding.py
@@ -15,8 +15,7 @@ class SentenceTransformerEmbedding(EmbeddingInterface):
 
     def embed_sentence(self, sentence: str):
         """
-        converts a sentence into glove embeddings
-        if words are not part of the dictionary they are mapped to the zero vector
+        converts a sentence into SBERT embeddings using the Sentence Transformers package
         ----------
         sentence : str
             the string that is to be embedded
@@ -26,5 +25,7 @@ class SentenceTransformerEmbedding(EmbeddingInterface):
         return embedding
     def return_hyper_params(self):
         hyper_params = {"embedding_dimension": self.model.get_sentence_embedding_dimension(),
-                        "model_name": self.model_name}
+                        "model_name": self.model_name,
+                        "sentence_features": self.model.get_sentence_features(),
+                        "parameters": self.model.get_parameter()}
         return hyper_params
