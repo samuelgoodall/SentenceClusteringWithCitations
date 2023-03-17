82addd62eaac39c958a10d6631dfee0fce6d21c1
esteli <emg96@uw.edu>
2023-03-17 19:05:43 +0100

    no changes made

--- experiments/embedding_methods/bert_embedding.py
+++ experiments/embedding_methods/bert_embedding.py
@@ -10,9 +10,9 @@ from transformers import BertTokenizer, BertModel
 class BertTransformerEmbedding(EmbeddingInterface):
 
     def __init__(self, model_name: str):
-        self.model = BertModel.from_pretrained("bert-base-uncased")
+        self.model = BertModel.from_pretrained(model_name)
         self.model_name = model_name
-        self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+        self.tokenizer = BertTokenizer.from_pretrained(model_name)
 
     def embed_sentence(self, sentence: str):
         """
@@ -21,9 +21,10 @@ class BertTransformerEmbedding(EmbeddingInterface):
         """
         encoded_input = self.tokenizer(sentence, return_tensors='pt')
         output = self.model(**encoded_input)
+
         return output
 
     def return_hyper_params(self):
-        hyper_params = {"embedding_dimension": ,
+        hyper_params = {"embedding_dimension": self.,
                         "model_name":}
         return hyper_params
--- experiments/evaluation.py
+++ experiments/evaluation.py
@@ -12,7 +12,7 @@ from dataset.customDataloader import get_dataloader
 from experiments.clustering_methods.clustering_interface import ClusteringInterface
 from experiments.clustering_methods.db_scan_clustering import DBScanClustering
 from experiments.embedding_methods.embedding_interface import EmbeddingInterface
-from experiments.embedding_methods.glove_embedding import GloveEmbedding
+from experiments.embedding_methods.bert_embedding import BertTransformerEmbedding
 
 
 class SentenceCitationFusingMethod(Enum):
@@ -111,8 +111,7 @@ def evaluate(embedding: EmbeddingInterface, clustering: ClusteringInterface):
 
 
 def main():
-    glove_embeddings_path = "../experiments/embedding_methods/embeddings/glove/glove.840B.300d.txt"
-    embedding = GloveEmbedding(300, glove_embeddings_path)
+    embedding = BertTransformerEmbedding("bert-base-uncased")
     clustering = DBScanClustering(eps=1.5, min_samples=1, metric="euclidean")
     evaluate(embedding, clustering)
 
