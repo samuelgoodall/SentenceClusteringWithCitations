4c2e58467363e08ac203ff936712dd407c4a88e6
esteli <emg96@uw.edu>
2023-03-21 16:51:12 +0100

    Adjusted Bert embedding class, removed the "[CLS]" and "[SEP]" tokens at the beginning and end of each sentence. Should work but seems to have a problem with DBSCAN. Added Tokenizer as a hyperparameter

--- experiments/embedding_methods/bert_embedding.py
+++ experiments/embedding_methods/bert_embedding.py
@@ -18,17 +18,16 @@ class BertTransformerEmbedding(EmbeddingInterface):
         sentence : str
             the string that is to be embedded
         """
-        # add special tokens at the beginning and end of each sentence
-        sentence = ["[CLS]" + sentence + "[SEP]"]
         tokenized_texts = self.tokenizer.tokenize(sentence)
         # use the BERT tokenizer to convert tokens to their index numbers
         input_ids = [self.tokenizer.convert_tokens_to_ids(x) for x in tokenized_texts]
         # take the average of the input ID's for each sentence
-        output = np.Average(input_ids)
+        output = np.average(input_ids)
 
         return output
 
     def return_hyper_params(self):
         hyper_params = {"Model name": self.model_name,
-                        "Padding Strategies": self.tokenizer._get_padding_truncation_strategies()}
+                        "Padding Strategies": self.tokenizer._get_padding_truncation_strategies(),
+                        "Tokenizer": self.tokenizer}
         return hyper_params
