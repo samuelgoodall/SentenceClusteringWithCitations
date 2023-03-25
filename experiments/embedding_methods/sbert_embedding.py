00d7aa448c86a9876ae06cef40faf17c6a1d36f3
esteli <emg96@uw.edu>
2023-03-25 17:19:57 +0100

    added better notation, such as parameter and hyperparameter notation

--- experiments/embedding_methods/sbert_embedding.py
+++ experiments/embedding_methods/sbert_embedding.py
@@ -10,13 +10,19 @@ from sentence_transformers import SentenceTransformer
 class SentenceTransformerEmbedding(EmbeddingInterface):
 
     def __init__(self, model_name: str):
+        """
+                Parameters
+                ----------
+                model_name : str
+                    The name of the model used
+                """
         self.model = SentenceTransformer(model_name)
         self.model.max_seq_length = 512
         self.model_name = model_name
 
     def embed_sentence(self, sentence: str):
         """
-        converts a sentence into SBERT embeddings using the Sentence Transformers package
+        Embeds the words in a sentence using the Sentence Transformers package (SBERT)
         ----------
         sentence : str
             the string that is to be embedded
@@ -25,6 +31,8 @@ class SentenceTransformerEmbedding(EmbeddingInterface):
         embedding = self.model.encode(sentence)
         return embedding
     def return_hyper_params(self):
+        """returns hyperparameters for this embedding model
+                        """
         hyper_params = {"embedding_dimension": self.model.get_sentence_embedding_dimension(),
                         "model_name": self.model_name,
                         "sentence_features": self.model.get_sentence_features(),
