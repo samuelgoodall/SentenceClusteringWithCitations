18782fe45d46d1641045ebdab25b507fd2f96790
Samuel Goodall <samuel.goodall@web.de>
2023-03-27 09:19:22 +0200

    -added gpu support -fixed bug with numpy shape

--- experiments/embedding_methods/bert_embedding.py
+++ experiments/embedding_methods/bert_embedding.py
@@ -11,27 +11,28 @@ class BertTransformerEmbedding(EmbeddingInterface):
 
     def __init__(self, model_name: str):
         """
-                Parameters
-                ----------
-                model_name : str
-                    The name of the BERT model to be used
-                """
-        self.model = BertModel.from_pretrained(model_name)
+        Parameters
+        ----------
+        model_name : str
+            The name of the BERT model to be used
+        """
+        self.device = "cuda:0" if torch.cuda.is_available() else "cpu"
+        self.model = BertModel.from_pretrained(model_name).to(self.device)
         self.model_name = model_name
         self.tokenizer = BertTokenizer.from_pretrained(model_name)
 
     def embed_sentence(self, sentence: str):
         """Returns a value for the sentence embedding by taking the pooled values of each token embedding.
 
-                Parameters
-                ----------
-                sentence : str
-                    The string that is to be embedded
-                """
+        Parameters
+        ----------
+        sentence : str
+            The string that is to be embedded
+        """
         # tokenize sentences
         encoding = self.tokenizer.encode_plus(sentence, add_special_tokens=True,
                                          truncation=True, padding="max_length",
-                                         return_attention_mask=True, return_tensors="pt")
+                                         return_attention_mask=True, return_tensors="pt").to(self.device)
         attention_mask = encoding["attention_mask"][0]
         output = self.model(**encoding)
 
@@ -41,7 +42,10 @@ class BertTransformerEmbedding(EmbeddingInterface):
         sum_embeddings = torch.sum(token_embeddings * input_mask_expanded, 1)
         sum_mask = torch.clamp(input_mask_expanded.sum(1), min=1e-9)
 
-        return sum_embeddings / sum_mask
+        sentence_embedding = (sum_embeddings / sum_mask)
+        numpy_embedding = sentence_embedding.detach().cpu().numpy()
+        numpy_embedding.resize((768,))
+        return numpy_embedding
 
     def return_hyper_params(self):
         """returns hyperparameters for this embedding model
