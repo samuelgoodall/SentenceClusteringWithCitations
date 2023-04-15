6c2a99b0b32ca4cbece82dee2ed47898fcf3d8c8
Samuel Goodall <samuel.goodall@web.de>
2023-04-15 14:05:01 +0200

    -added support for test,train,validation split that is reproducable

--- dataset/customDataloader.py
+++ dataset/customDataloader.py
@@ -1,8 +1,8 @@
 import sqlite3
 import time
 
-import torch
-from torch.utils.data import Dataset, DataLoader, random_split
+from torch import Generator
+from torch.utils.data import Dataset, DataLoader, random_split, Subset
 
 from dataset.database.database import SQAlchemyDatabase, Paper
 from dataclasses import dataclass
@@ -98,10 +98,11 @@ class ArxivDataset(Dataset):
             if sentence_id not in sentences:
                 sentence = item[1]
                 sentences[sentence_id] = DataItemTO(paragraph_id=paragraph_id, sentence=sentence, citations=list())
-            sentences[sentence_id].citations.append(CitationItemTO(citation_title=item[2], citation_abstract=item[3] if (item[3] is not None) else ""))
+            sentences[sentence_id].citations.append(
+                CitationItemTO(citation_title=item[2], citation_abstract=item[3] if (item[3] is not None) else ""))
 
         sentences = list(sentences.values())
-        labels = list(map(lambda dataItem: dataItem.paragraph_id ,sentences))
+        labels = list(map(lambda dataItem: dataItem.paragraph_id, sentences))
         return sentences, labels
 
 
@@ -122,7 +123,7 @@ def custom_collate(batch: list):
     return batch
 
 
-def get_dataloader(batch_size: int = 20, shuffle: bool = False, dataset : ArxivDataset = None):
+def get_dataloader(batch_size: int = 20, shuffle: bool = False, dataset: ArxivDataset = None):
     """
     gets the dataloader on the whole dataset
 
@@ -144,7 +145,7 @@ def get_dataloader(batch_size: int = 20, shuffle: bool = False, dataset : ArxivD
 
 def get_train_test_validation_dataloader(batch_size: int = 20, shuffle: bool = True,
                                          train_test_validation_split: list[float] = [],
-                                         fixed_random_seed: int = None, dataset: ArxivDataset = None):
+                                         fixed_random_generator: Generator = None, dataset: ArxivDataset = None):
     """
     gets the dataloaders for train test and validation
 
@@ -156,8 +157,8 @@ def get_train_test_validation_dataloader(batch_size: int = 20, shuffle: bool = T
         Sets wether dataloader shall shuffle the data whilst iterating
     train_test_validation_split : list[float]
         The ratio of train test and validation split of the whole dataset, list of floats has to sum to 1
-    fixed_random_seed : int
-        seed that can be set for reproducable results
+    fixed_random_generator : Generator
+        generator that can be set for reproducable results
 
     Returns
     -------
@@ -167,8 +168,7 @@ def get_train_test_validation_dataloader(batch_size: int = 20, shuffle: bool = T
     Raises
     ------
     ValueError
-        If no sound is set for the animal or passed in as a
-        parameter.
+        If train_test_validation split is malformed
     """
 
     if len(train_test_validation_split) < 3 or train_test_validation_split is None:
@@ -177,19 +177,96 @@ def get_train_test_validation_dataloader(batch_size: int = 20, shuffle: bool = T
     if sum(train_test_validation_split) != 1.0:
         raise ValueError("train_test_validation_split values have to sum up to 1!")
 
-    if fixed_random_seed is None:
+    if fixed_random_generator is None:
         train_dataset, test_dataset, validation_dataset = random_split(dataset,
                                                                        train_test_validation_split)
     else:
         train_dataset, test_dataset, validation_dataset = random_split(dataset,
                                                                        train_test_validation_split,
-                                                                       generator=torch.Generator().manual_seed(
-                                                                           fixed_random_seed))
+                                                                       generator=fixed_random_generator)
     train_dataloader = DataLoader(train_dataset, batch_size=batch_size,
                                   shuffle=shuffle, collate_fn=custom_collate)
     test_dataloader = DataLoader(test_dataset, batch_size=batch_size,
                                  shuffle=shuffle, collate_fn=custom_collate)
     validation_dataloader = DataLoader(validation_dataset, batch_size=batch_size,
+                                       shuffle=shuffle, collate_fn=custom_collate)
+
+    return train_dataloader, test_dataloader, validation_dataloader
+
+
+def get_train_test_validation_index_split(train_test_validation_split: list[float] = [],
+                                          fixed_random_generator: Generator = None, dataset: ArxivDataset = None):
+    """
+    gets the train,test,validation indexes needed for reproducable datasetsplits
+
+    Parameters
+    ----------
+    train_test_validation_split : list[float]
+        The ratio of train test and validation split of the whole dataset, list of floats has to sum to 1
+    fixed_random_generator : Generator
+        generator that can be set for reproducable results
+    dataset: ArxivDataset
+            the Dataset to be partitioned
+
+    Returns
+    -------
+    tuple[list[int]]
+        the train,test and validation index lists
+
+    Raises
+    ------
+    ValueError
+        If train_test_validation split is malformed
+    """
+
+    if len(train_test_validation_split) < 3 or train_test_validation_split is None:
+        raise ValueError("train_test_validation_split must be given! has to be array/list of floats that sum up to 1!")
+
+    if sum(train_test_validation_split) != 1.0:
+        raise ValueError("train_test_validation_split values have to sum up to 1!")
+
+    if fixed_random_generator is None:
+        train_dataset, test_dataset, validation_dataset = random_split(dataset,
+                                                                       train_test_validation_split)
+    else:
+        train_dataset, test_dataset, validation_dataset = random_split(dataset,
+                                                                       train_test_validation_split,
+                                                                       generator=fixed_random_generator)
+    return train_dataset.indices, test_dataset.indices, validation_dataset.indices
+
+
+def get_train_test_validation_split_indexbased_dataloader(train_idx=[], test_idx=[], val_idx=[], batch_size: int = 20,
+                                                          shuffle: bool = True, dataset: ArxivDataset = None):
+    """
+    gets the dataloaders for train test and validation based on three arrays with indexes
+
+    Parameters
+    ----------
+    train_idx: list<int>
+        the indexes belonging to the train subset of the dataset
+    test_idx: list<int>
+        the indexes belonging to the test subset of the dataset
+    val_idx: list<int>
+        the indexes belonging to the validation subset of the dataset
+    batch_size : int
+        The size of the batch = how many examples are fetched
+    shuffle : bool
+        Sets wether dataloader shall shuffle the data whilst iterating
+    dataset: ArxivDataset
+        the Dataset to be partitioned
+
+    Returns
+    -------
+    tuple[DataLoader]
+        the train,test and validation dataloader
+
+    """
+
+    train_dataloader = DataLoader(Subset(dataset, train_idx), batch_size=batch_size,
+                                  shuffle=shuffle, collate_fn=custom_collate)
+    test_dataloader = DataLoader(Subset(dataset, test_idx), batch_size=batch_size,
+                                 shuffle=shuffle, collate_fn=custom_collate)
+    validation_dataloader = DataLoader(Subset(dataset, val_idx), batch_size=batch_size,
                                        shuffle=shuffle, collate_fn=custom_collate)
 
     return train_dataloader, test_dataloader, validation_dataloader
--- experiments/experiments_execution.py
+++ experiments/experiments_execution.py
@@ -1,8 +1,11 @@
+import torch
 from torch.utils.data import DataLoader
 
-from dataset.customDataloader import get_dataloader, ArxivDataset, get_train_test_validation_dataloader
+from dataset.customDataloader import ArxivDataset, get_train_test_validation_index_split, \
+    get_train_test_validation_split_indexbased_dataloader
 from dataset.customDataloaderPrecomputedEmbeddings import ArxivDatasetPrecomputedEmbeddings
 from experiments.clustering_methods.db_scan_clustering import DBScanClustering
+from experiments.clustering_methods.gmm_clustering import GMMClustering
 from experiments.clustering_methods.spherical_kmeans_clustering import SphericalKMeansClustering
 from experiments.embedding_methods.bert_embedding import BertTransformerEmbedding
 from experiments.embedding_methods.embedding_interface import EmbeddingInterface
@@ -43,10 +46,10 @@ def conduct_experiment_precomputed_embedding(embedding: EmbeddingInterface, data
     dataloader: DataLoader
         the dataloader for the dataset to be iterated
     """
-    # gmm_clustering = GMMClustering()
+    gmm_clustering = GMMClustering()
     db_scan_clustering = DBScanClustering()
     spherical_kmeans_clustering = SphericalKMeansClustering()
-    clustering_methods = [spherical_kmeans_clustering, db_scan_clustering]
+    clustering_methods = [gmm_clustering, spherical_kmeans_clustering, db_scan_clustering]
     for clustering in clustering_methods:
         evaluate_with_precomputed_embeddings(embedding=embedding, clustering=clustering,
                                              dataloader=dataloader, use_citation=True)
@@ -58,6 +61,9 @@ def main():
     """
     Script used for conducting the experiments
     """
+
+    generator = torch.Generator().manual_seed(42)
+
     glove_embeddings_path = "../experiments/embedding_methods/embeddings/glove/glove.42B.300d.txt"
     glove_embedding = GloveEmbedding(300, glove_embeddings_path)
     fastText_embedding = FastTextEmbedding(300, "../experiments/embedding_methods/embeddings/FastText/cc.en.300.bin")
@@ -67,22 +73,37 @@ def main():
     # is one at the moment makes iterating easier, batch size of 200 would save some seconds of execute
     batch_size = 1
     unlemmatized_dataset = ArxivDataset("../dataset/database/dataset_new.db")
-    unlemmatized_dataloader = get_dataloader(batch_size, shuffle=False,
-                                             dataset=unlemmatized_dataset)
+
+    # the train, test and validation indexes have to be the same for all experiments in order to have comparability
+    train_indexes, test_indexes, validation_indexes = get_train_test_validation_index_split(
+        train_test_validation_split=[0.8, 0.2, 0.0],
+        fixed_random_generator=generator, dataset=unlemmatized_dataset)
+
+    unlemmatized_dataloader_train, unlemmatized_dataloader, unlemmatized_dataloader_validation = get_train_test_validation_split_indexbased_dataloader(
+        train_idx=train_indexes, test_idx=test_indexes, val_idx=validation_indexes, batch_size=batch_size,
+        shuffle=False, dataset=unlemmatized_dataset)
+
     lemmatized_dataset = ArxivDataset("../dataset/database/dataset_new_lemmatized.db")
-    lemmatized_dataloader = get_dataloader(batch_size, shuffle=False,
-                                           dataset=lemmatized_dataset)
+    lemmatized_dataloader_train, lemmatized_dataloader, lemmatized_dataloader_validation = get_train_test_validation_split_indexbased_dataloader(
+        train_idx=train_indexes, test_idx=test_indexes, val_idx=validation_indexes, batch_size=batch_size,
+        shuffle=False,
+        dataset=lemmatized_dataset)
+
     sbert_dataset = ArxivDatasetPrecomputedEmbeddings("../dataset/database/dataset_new_precomputed_embeddings_sbert.db")
-    sbert_dataloader = get_dataloader(batch_size, shuffle=False,
-                                      dataset=sbert_dataset)
+    sbert_dataloader_train, sbert_dataloader, sbert_dataloader_validation = get_train_test_validation_split_indexbased_dataloader(
+        train_idx=train_indexes, test_idx=test_indexes, val_idx=validation_indexes, batch_size=batch_size,
+        shuffle=False,
+        dataset=sbert_dataset)
+
     bert_dataset = ArxivDatasetPrecomputedEmbeddings("../dataset/database/dataset_new_precomputed_embeddings_bert.db")
-    bert_dataloader = get_dataloader(batch_size, shuffle=False,
-                                     dataset=bert_dataset)
+    bert_dataloader_train, bert_dataloader, bert_dataloader_validation = get_train_test_validation_split_indexbased_dataloader(
+        train_idx=train_indexes, test_idx=test_indexes, val_idx=validation_indexes, batch_size=batch_size,
+        shuffle=False,
+        dataset=bert_dataset)
     conduct_experiment_precomputed_embedding(embedding=bert_embedding, dataloader=bert_dataloader)
     conduct_experiment_precomputed_embedding(embedding=sbert_embedding, dataloader=sbert_dataloader)
     conduct_experiment(embedding=glove_embedding, dataloader=lemmatized_dataloader)
     conduct_experiment(embedding=fastText_embedding, dataloader=unlemmatized_dataloader)
-
 
 
 if __name__ == "__main__":
